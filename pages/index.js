f8d08fcd6752823278376d5b1947d7d52eb6b263
Deniz Orsel <denizonerorsel@gmail.com>
2024-02-19 09:13:15 +0100

    add mail link

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,9 @@ export default function Home() {
       </div>
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-5xl md:text-9xl font-bold">Deniz Orsel</h1>
-        <p className="md:text-4xl font-light">denizonerorsel@gmail.com</p>
+        <p className="md:text-4xl font-light">
+          <a href='mailto:denizonerorsel@gmail.com'>denizonerorsel@gmail.com</a>
+        </p>
       </div>
     </main>
   );
