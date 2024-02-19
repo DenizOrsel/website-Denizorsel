21f99e7332ea8eb9fdd621f7d26a231449288aa6
Deniz Orsel <denizonerorsel@gmail.com>
2024-02-19 09:20:18 +0100

    update underline

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-5xl md:text-9xl font-bold">Deniz Orsel</h1>
         <p className="md:text-4xl font-light">
-          <a href='mailto:denizonerorsel@gmail.com'>denizonerorsel@gmail.com</a>
+          <a href='mailto:denizonerorsel@gmail.com' className="hover:underline">denizonerorsel@gmail.com</a>
         </p>
       </div>
     </main>
